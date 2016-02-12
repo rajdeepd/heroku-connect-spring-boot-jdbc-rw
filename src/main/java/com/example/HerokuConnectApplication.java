1c05e4cf7d4e6f433fe94a4c9ceed5a0485b7db9
Rajdeep Dua <dua_rajdeep@yahoo.com>
2016-02-12 11:53:28 +0530

    changed indentation

--- src/main/java/com/example/HerokuConnectApplication.java
+++ src/main/java/com/example/HerokuConnectApplication.java
@@ -2,7 +2,6 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.web.bind.annotation.*;
 import org.springframework.stereotype.*;
 import java.sql.*;
 import java.io.*;
@@ -11,7 +10,6 @@ import java.net.URISyntaxException;
 import java.net.URI;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
@@ -20,32 +18,33 @@ public class HerokuConnectApplication {
 
 
 	@RequestMapping("/contacts")
-	public String contacts( Model model) {
-		try {
-			Connection connection  = getConnection();
-			Statement stmt = connection.createStatement();
-			String sql;
-			sql = "SELECT id, sfid,  firstname, lastname, name, email FROM salesforce.contact";
-			ResultSet rs = stmt.executeQuery(sql);
-			StringBuffer sb = new StringBuffer();
+    public String contacts(Model model) {
+        try {
+            Connection connection = getConnection();
+            Statement stmt = connection.createStatement();
+            String sql;
+            sql = "SELECT id, sfid,  firstname, lastname, name, email FROM salesforce.contact";
+            ResultSet rs = stmt.executeQuery(sql);
+            StringBuffer sb = new StringBuffer();
             List contacts = new ArrayList<>();
-			// Extract data from result set
-			while(rs.next()){
-				int id  = rs.getInt("id");
+            // Extract data from result set
+            while (rs.next()) {
+                int id = rs.getInt("id");
                 String sfid = rs.getString("sfid");
-				String name = rs.getString("name");
+                String name = rs.getString("name");
                 String first = rs.getString("firstname");
                 String last = rs.getString("lastname");
                 String email = rs.getString("email");
                 contacts.add(new Contact(id, sfid, first, last, email));
-			}
+            }
             model.addAttribute("contacts", contacts);
             return "contact";
-		}catch(Exception e) {
-			return e.toString();
-		}
-	}
-	private static Connection getConnection() throws URISyntaxException, SQLException {
+        } catch (Exception e) {
+            return e.toString();
+        }
+    }
+
+    private static Connection getConnection() throws URISyntaxException, SQLException {
 		URI dbUri = new URI(System.getenv("DATABASE_URL"));
 
 		String username = dbUri.getUserInfo().split(":")[0];
